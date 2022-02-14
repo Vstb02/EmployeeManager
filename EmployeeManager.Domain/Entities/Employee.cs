4b4e35d67b80ea0f7bde05776f6004a5a6c22f28
Judori <steblevvlad037@gmail.com>
2022-02-14 15:08:23 +0300

    Добавьте файлы проекта.

---
+++ EmployeeManager.Domain/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#pragma warning disable
+
+namespace EmployeeManager.Domain.Entities
+{
+    public class Employee : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronymic { get; set; }
+        public DateTime? Birthday { get; set; }
+        public byte[]? Avatar { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
